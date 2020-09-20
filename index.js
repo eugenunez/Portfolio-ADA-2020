71fde9ee8c586a4bab1a2ab5adca9738ee070256
Eugenia Nunez <eugeiq@gmail.com>
2020-09-20 14:25:48 -0300

    Se agrega archivos js

---
+++ index.js
@@ -0,0 +1,11 @@
+const tarjetas = document.querySelectorAll(".tarjeta-proyectos");
+const botonesFiltro = document.querySelectorAll(".link-pro");
+
+for (let boton of botonesFiltro){ 
+boton.onclick = () =>
+for (let tarjeta of tarjetas){
+
+
+
+    
+}

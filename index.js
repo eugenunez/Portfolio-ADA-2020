757f3adffdf3089405aebd678bbe692cde6f8da8
Eugenia Nunez <eugeiq@gmail.com>
2020-09-20 23:00:41 -0300

    Agregue menu desplegable responsive y fitros en mis proyectos

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const tarjetas = document.querySelectorAll(".tarjeta-proyectos");
 const botonesFiltro = document.querySelectorAll(".link-pro");
+const hamburguesa=document.getElementById("hamburguesa");
+const menu=document.querySelector(".menu-responsive");
 
-for (let boton of botonesFiltro){ 
-boton.onclick = () =>
-for (let tarjeta of tarjetas){
+hamburguesa.onclick=()=>{
+    menu.classList.toggle("mostrar-menu")
+}
 
+for (let boton of botonesFiltro) {
+    boton.onclick = () => {
+        for (let tarjeta of tarjetas) {
+            if (boton.dataset.tecnologia === tarjeta.dataset.tecnologia) {
+                tarjeta.classList.remove("hidden")
+            }
+            else if (boton.dataset.tecnologia==="todos") {
+                tarjeta.classList.remove("hidden")
+            }
+            else {
+                tarjeta.classList.add("hidden")
+            }
 
-
-    
+        }
+    }
 }
